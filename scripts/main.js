02c9b07e64a491bab66270d5697ecd795638dd43
zero <zero@gmail.com>
2017-08-25 22:34:16 +0800

    first

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+//广告轮播
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true
+    });
+});
+
+
+
+
